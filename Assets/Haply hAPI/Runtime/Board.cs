7a032d4b85be9a0860b46f634c8622b1c4277e43
Rishav Banerjee <thatamuzak@gmail.com>
2024-02-18 12:13:56 -0800

    Base project with working force feedback

---
+++ Assets/Haply hAPI/Runtime/Board.cs
@@ -0,0 +1,205 @@
+﻿using UnityEngine;
+using System.IO.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Haply.hAPI
+{
+    public class Board : MonoBehaviour
+    {
+        [SerializeField] protected int m_BaudRate;
+        [SerializeField] protected int m_SerialTimeout;
+        [HideInInspector] public string targetPort;
+
+        private SerialPort Port { get;  set; }
+        protected bool m_HasBeenInitialized;
+
+        public virtual void Initialize ()
+        {
+            if ( m_HasBeenInitialized )
+            {
+                Debug.Log( "Board Already Initialized" );
+                return;
+            }
+            
+            try
+            {
+                Port = new SerialPort( targetPort, m_BaudRate );
+
+                Port.ReadTimeout = m_SerialTimeout;
+                Port.WriteTimeout = m_SerialTimeout;
+                Port.DtrEnable = true;
+                Port.RtsEnable = true;
+
+                Port.Open();
+
+                Debug.Log( "Initialized Board" );
+                Debug.Log( Port.IsOpen );
+
+                m_HasBeenInitialized = true;
+            }
+            catch ( Exception exception )
+            {
+                Debug.LogException( exception );
+            }
+        }
+
+        public virtual void ClosePort ()
+        {
+            Port.Close();
+
+            m_HasBeenInitialized = false;
+            Debug.Log( "Port closed" );
+        }
+
+        public string [] GetAvailablePorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+        
+        private void OnDestroy ()
+        {
+            if ( m_HasBeenInitialized || (Port != null && Port.IsOpen) )
+            {
+                ClosePort();
+            }
+        }
+        
+
+        /**
+         * Formats and transmits data over the serial port
+         * 
+         * @param	 communicationType type of communication taking place
+         * @param	 deviceID ID of device transmitting the information
+         * @param	 bData byte inforamation to be transmitted
+         * @param	 fData float information to be transmitted
+         */
+        public virtual void Transmit ( byte communicationType, byte deviceID, byte[] bData, float[] fData )
+        {
+            byte[] outData = new byte[2 + bData.Length + 4 * fData.Length];
+            byte[] segments = new byte[4];
+
+            outData[0] = communicationType;
+            outData[1] = deviceID;
+
+            Array.Copy( bData, 0, outData, 2, bData.Length );
+
+            int j = 2 + bData.Length;
+            for ( int i = 0; i < fData.Length; i++ )
+            {
+                segments = FloatToBytes( fData[i] );
+                Array.Copy( segments, 0, outData, j, 4 );
+                j = j + 4;
+            }
+
+            Port.Write( outData, 0, outData.Length );
+        }
+
+        /**
+         * Receives data from the serial port and formats data to return a float data array
+         * 
+         * @param	 type type of communication taking place
+         * @param	 deviceID ID of the device receiving the information
+         * @param	 expected number for floating point numbers that are expected
+         * @return	formatted float data array from the received data
+         */
+        public virtual float[] Receive ( byte communicationType, byte deviceID, int expected )
+        {
+            //Set_buffer(1 + 4 * expected);
+
+            byte[] segments = new byte[4];
+
+            byte[] inData = new byte[1 + 4 * expected];
+            float[] data = new float[expected];
+
+            Port.Read( inData, 0, inData.Length );
+
+            if ( inData[0] != deviceID )
+            {
+                //Debug.LogError("Error, another device expects this data!");
+            }
+
+            int j = 1;
+
+            for ( int i = 0; i < expected; i++ )
+            {
+                Array.Copy( inData, j, segments, 0, 4 );
+                data[i] = BytesToFloat( segments );
+                j = j + 4;
+            }
+
+            return data;
+        }
+
+        /**
+         * @return   a boolean indicating if data is available from the serial port
+         */
+        public virtual bool DataAvailable ()
+        {
+            bool available = false;
+
+            if ( Port.BytesToRead > 0 )
+            {
+                available = true;
+            }
+
+            return available;
+        }
+
+        /**
+         * Sends a reset command to perform a software reset of the Haply board
+         *
+         */
+        private void ResetBoard ()
+        {
+            byte communicationType = 0;
+            byte deviceID = 0;
+            byte[] bData = new byte[0];
+            float[] fData = new float[0];
+
+            Transmit( communicationType, deviceID, bData, fData );
+        }
+
+        /**
+         * Set serial buffer length for receiving incoming data
+         *
+         * @param   length number of bytes expected in read buffer
+         */
+        private void SetBuffer ( int length )
+        {
+            Port.ReadBufferSize = length;
+        }
+
+        /**
+         * Translates a float point number to its raw binary format and stores it across four bytes
+         *
+         * @param	val floating point number
+         * @return   array of 4 bytes containing raw binary of floating point number
+         */
+        protected byte[] FloatToBytes ( float val )
+        {
+            return BitConverter.GetBytes( val );
+        }
+
+        /**
+         * Translates a binary of a float point to actual float point
+         *
+         * @param	segment array containing raw binary of floating point
+         * @return   translated floating point number
+         */
+        protected float BytesToFloat ( byte[] segment )
+        {
+            return BitConverter.ToSingle( segment, 0 );
+        }
+
+        public static byte[] SubArray ( byte[] data, int index, int length )
+        {
+            byte[] result = new byte[length];
+
+            Array.Copy( data, index, result, 0, length );
+
+            return result;
+        }
+    }
+}
---
+++ Assets/Haply hAPI/Runtime/Editor/BoardCustomEditor.cs
@@ -0,0 +1,31 @@
+﻿using Haply.hAPI;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Board))][InitializeOnLoad]
+public class BoardCustomEditor : Editor
+{
+    private int targetPortIndex = 0;
+    private SerializedProperty portProperty;
+    public override void OnInspectorGUI()
+    {
+        Board board = target as Board;
+
+        string[] availablePorts = board.GetAvailablePorts();
+
+        if (availablePorts.Length > 0)
+        {
+            portProperty = serializedObject.FindProperty("targetPort");
+            targetPortIndex = EditorGUILayout.Popup("Active Ports", targetPortIndex, availablePorts);
+            portProperty.stringValue = availablePorts[targetPortIndex];
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("No available devices detected, please enter port manually", MessageType.Info);
+            portProperty = serializedObject.FindProperty("targetPort");
+            EditorGUILayout.PropertyField(portProperty, new GUIContent("Port"));
+        }
+        serializedObject.ApplyModifiedProperties();
+        DrawDefaultInspector();
+    }
+}
