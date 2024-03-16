3f38c96b5483bf43498e6fcbbabd26fbf2c25215
Rishav Banerjee <rishavbanerjee1710@gmail.com>
2024-03-15 19:04:12 -0700

    camera centering implemented

---
+++ Assets/_Scripts/TrackEndEffector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TrackEndEffector : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform endEffectorRepresentation;
+
+    public void CenterEndEffector()
+    {
+        Vector3 camForward = transform.forward;
+        Vector3 eePosition = endEffectorRepresentation.position;
+        float dist = Vector3.Distance(transform.position, eePosition);
+        Vector3 targetPoint = eePosition + -camForward * dist;
+        StartCoroutine(MoveCoroutine(targetPoint));
+    }
+    
+    IEnumerator MoveCoroutine(Vector3 targetPosition)
+    {
+        float distance = Vector3.Distance(transform.position, targetPosition);
+
+        while (distance > 0.001f)
+        {
+            Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            transform.position = newPosition;
+            distance = Vector3.Distance(transform.position, targetPosition);
+            yield return null;
+        }
+    }
+}
