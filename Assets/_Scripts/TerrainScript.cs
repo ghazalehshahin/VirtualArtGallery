56f021be06f751753eab9bf82b56a09e40ec77e8
PierreMARUEJOL <pierre.maruejol@gmail.com>
2024-03-20 02:55:12 -0400

    feat: object painting working

--- Assets/_Scripts/TerrainScript.cs
+++ Assets/_Scripts/TerrainScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TerrainScript : MonoBehaviour
 {
@@ -10,6 +12,9 @@ public class TerrainScript : MonoBehaviour
     [SerializeField] private float brushSize = 5f;
     [SerializeField] private float brushStrength = 1f;
     [SerializeField] private AnimationCurve brushCurve;
+
+    [SerializeField] private int treePrototypeIndex;
+    [SerializeField] private float offsetRadius = 0.75f;
 
 
     void Start()
@@ -76,6 +81,56 @@ public class TerrainScript : MonoBehaviour
         terrainData.SetAlphamaps(startX, startY, splatmapData);
     }
 
+    void PaintTree(Vector3 worldPosition)
+    {
+
+        TerrainData terrainData = terrain.terrainData;
+        // Define the radius within which trees will be randomly placed
+        float placementRadius = 0f; // Adjust this value as needed
+
+        // Calculate a random offset within the placement radius
+        Vector2 randomOffset = Random.insideUnitCircle * placementRadius;
+
+        // Calculate the new position by adding the random offset to the clicked position
+        Vector3 newPosition = worldPosition + new Vector3(randomOffset.x, 0f, randomOffset.y);
+
+        // Convert world position to terrain local position
+        Vector3 terrainLocalPos = newPosition - terrain.transform.position;
+
+        // Calculate the terrain cell coordinates
+        int terrainX = (int)((terrainLocalPos.x / terrainData.size.x) * terrainData.heightmapResolution);
+        int terrainZ = (int)((terrainLocalPos.z / terrainData.size.z) * terrainData.heightmapResolution);
+
+
+        if (terrainX >= 0 && terrainX < terrainData.heightmapResolution &&
+            terrainZ >= 0 && terrainZ < terrainData.heightmapResolution)
+        {
+            // Get the terrain height at the given coordinates
+            float terrainHeight = terrainData.GetHeight((int)terrainX, (int)terrainZ);
+
+            // Set the position for placing the tree
+            //Vector3 treePosition = new Vector3(terrainLocalPos.x, terrainHeight, terrainLocalPos.z);
+
+            // Create a new TreeInstance
+            TreeInstance newTree = new TreeInstance();
+            newTree.position = new Vector3(((float)terrainX * terrainData.heightmapScale.x + Random.Range(-offsetRadius, offsetRadius)) / terrainData.size.x,
+                                            terrainHeight / terrainData.size.y, 
+                                            ((float)terrainZ * terrainData.heightmapScale.z + Random.Range(-offsetRadius, offsetRadius)) / terrainData.size.z
+                                          );
+            Debug.Log(newTree.position.x);
+            newTree.prototypeIndex = treePrototypeIndex;
+            newTree.widthScale = 2;
+            newTree.heightScale = 2;
+            newTree.color = Color.white;
+            newTree.lightmapColor = Color.white;
+
+            // Add the TreeInstance to the terrain data
+            terrain.AddTreeInstance(newTree);
+        }
+
+
+    }
+
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -86,6 +141,16 @@ public class TerrainScript : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 PaintTexture(hit.point);
+            }
+        }
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                PaintTree(hit.point);
             }
         }
     }
